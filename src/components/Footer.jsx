79bce5dc2772076c9fcb1bd474c4d87eb844f17b
Taryn King <marshaeking@gmail.com>
2023-01-27 22:49:28 -0600

    style: style footer to meet wireframe requirements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer>
       <div>
         <h5>Hit us up on our socials!</h5>
-        <div>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImFacebook size="1.5em" color="#3f3f3f" />
-          </a>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImInstagram size="1.5em" color="#3f3f3f" />
-          </a>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImTwitter size="1.5em" color="#3f3f3f" />
-          </a>
-          <a href="https://www.devmountain.com" target="_blank">
-            <ImGithub size="1.5em" color="#3f3f3f" />
-          </a>
+        <div className={styles.socialLinks}>
+            <a href="https://www.devmountain.com" rel="noreferrer" target="_blank">
+              <div>
+                <ImFacebook size="1.5em" color="#3f3f3f" />
+              </div>
+            </a>
+            <a href="https://www.devmountain.com" rel="noreferrer" target="_blank">
+              <div>
+                <ImInstagram size="1.5em" color="#3f3f3f" />
+              </div>
+            </a>
+            <a href="https://www.devmountain.com" rel="noreferrer" target="_blank">
+              <div>
+                <ImTwitter size="1.5em" color="#3f3f3f" />
+              </div>
+            </a>
+            <a href="https://www.devmountain.com" rel="noreferrer" target="_blank">
+              <div>
+                <ImGithub size="1.5em" color="#3f3f3f" />
+              </div>
+            </a>
         </div>
       </div>
       <svg

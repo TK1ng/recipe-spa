1b9714d4841c0d745a9508bbd1010b33dfad4c21
Taryn King <marshaeking@gmail.com>
2023-01-28 00:28:35 -0600

    style: add styling for adBanner component

--- src/components/homeComponents/AdBanner.jsx
+++ src/components/homeComponents/AdBanner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import salmon from "../../assets/salmon.jpg";
 import { Link } from "react-router-dom";
+import styles from "./AdBanner.module.css";
 
 const AdBanner = () => {
   return (
-    <div
+    <div className={styles.banner}
       style={{
         background: `linear-gradient(
           190deg,
@@ -14,15 +15,15 @@ const AdBanner = () => {
         backgroundSize: "cover",
       }}
     >
-      <div>
+      <div className={styles.bannerContent}>
         <h3>New Recipe</h3>
         <h1>Pineapple Salmon</h1>
         <h3>
           This recipe consists of fresh wild Alaskan salmon, rubbed in a bbq
-          brown sugar rub, baked for 25 minutes on a bed of pineapple, and garnished in butter, garlic, and chives. You won’t want to miss it!
+          brown sugar rub, baked for 25 minutes on a bed of pineapple, and garnished in butter, garlic, and chives. You won't want to miss it!
         </h3>
         <Link to="/recipe/3">
-          <button className="blue-btn">Check it out</button>
+          <button className={styles.blueBtn}>Check it out</button>
         </Link>
       </div>
     </div>

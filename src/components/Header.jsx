a4439b0da68b2a760f5e9043405ec47cfdcdf1fe
Taryn King <marshaeking@gmail.com>
2023-01-27 21:59:56 -0600

    style: style header to match wireframe requirements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import styles from "./Header.module.css";
 
 
 const Header = () => {
   return (
-    <header>
+    <header className={styles.topHeader}>
       <h2>Devmountain Eatery</h2>
-      <nav>
+      <nav className={styles.navBar}>
           <Link to="/"><button>Home</button></Link>
           <Link to="/new-recipe"><button>Add Recipe</button></Link>
       </nav>

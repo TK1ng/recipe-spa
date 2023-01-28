4f7f47a53af32cbe2f83f031cebc85f65457c6b1
Taryn King <marshaeking@gmail.com>
2023-01-28 13:38:19 -0600

    style: adjust banner opacity

--- src/components/homeComponents/AdBanner.jsx
+++ src/components/homeComponents/AdBanner.jsx
@@ -9,8 +9,8 @@ const AdBanner = () => {
       style={{
         background: `linear-gradient(
           190deg,
-          rgba(0, 0, 0, 0.8),
-          rgba(0, 0, 0, 0.8)),
+          rgba(0, 0, 0, 0.7),
+          rgba(0, 0, 0, 0.7)),
           url(${salmon})`,
         backgroundSize: "cover",
       }}

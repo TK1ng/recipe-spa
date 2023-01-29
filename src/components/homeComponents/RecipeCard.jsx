1524d3851775417fdc034a8a47121afe7e5479ee
Taryn King <marshaeking@gmail.com>
2023-01-29 08:52:16 -0600

    refactor: make recipe component dynamic to programmatically generate recipe cards

--- src/components/homeComponents/RecipeCard.jsx
+++ src/components/homeComponents/RecipeCard.jsx
@@ -1,10 +1,10 @@
 import styles from "./RecipeCard.module.css";
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipe_name, image_url }) => {
     return ( 
         <div className={styles.recipeCard}>
-            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sweet-potato-falafel-healthy-vegan-recipes-1596800863.jpg" alt="vegan falafel plated with couscous, pickled red cabbage, cucumbers and lemon slice" />
-            <p>Vegan falafel and couscous</p>
+            <img src={image_url} alt={recipe_name}/>
+            <p>{recipe_name}</p>
             <button className="blueBtn">
                 See More
             </button>

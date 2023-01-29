9de325a628f0f406d26c841fc4e513d53f2fc38e
Taryn King <marshaeking@gmail.com>
2023-01-29 00:41:56 -0600

    feat: add search functionality

--- src/components/homeComponents/HomeScreen.jsx
+++ src/components/homeComponents/HomeScreen.jsx
@@ -1,11 +1,28 @@
-import { findByLabelText } from '@testing-library/react';
+import { BsSearch } from 'react-icons/bs';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import AdBanner from './AdBanner';
 import RecipeCard from './RecipeCard';
+import Search from './Search';
 
 const HomeScreen = () => { 
   const [recipes, setRecipes] = useState([]);
+
+  console.log(recipes);
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const inputRef = useRef(null);
+
+  const handleSearchInput = (e) => {
+    console.log(e.target.value);
+
+    let filtered = recipes.filter(recipe => {
+      return recipe.recipe_name === searchTerm;
+    })
+
+    console.log(filtered)
+  }
 
   const getRecipes = () => {
     axios.get('https://recipes.devmountain.com/recipes')
@@ -25,6 +42,10 @@ const HomeScreen = () => {
     <div>
       <AdBanner />
       <main className="contentBody">
+        <div className="search">
+          <BsSearch style={{ color: '#f88213'}} />
+          <input ref={inputRef} placeholder="Search recipes" onChange={handleSearchInput}/>
+        </div>
         <RecipeCard />
       </main>
     </div>
---
+++ src/components/homeComponents/Search.jsx
@@ -0,0 +1,13 @@
+import { BsSearch } from 'react-icons/bs';
+import styles from './Search.module.css';
+
+const Search = () => {
+    return ( 
+        <div>
+            <BsSearch />
+            <input />
+        </div>
+     );
+}
+ 
+export default Search;
